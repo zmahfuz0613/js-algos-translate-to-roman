78f4ed375cfdbc587c33703cc24363ed3d4f8607
mickmed <mick2090@gmail.com>
2020-02-10 21:08:50 -0500

    updates

--- groupArrays.js
+++ groupArrays.js
@@ -1,5 +1,5 @@
 //GROUP ARRAYS
-//"groups the arrays into an array of arrays of given length, any remainding go in last array"
+//groups the arrays into an array of arrays of given length, any remainding go in last array
 
 const groupArrays = (arr, size) => {
   let groups = []
