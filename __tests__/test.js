8c18e192414f329a2a7933597dcfd51026dd97ad
mickmed <mick2090@gmail.com>
2020-02-10 19:03:04 -0500

    updates

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,11 +1,15 @@
-let algos = require("../groupArrays.js");
+let algos = require("../groupArrays.js")
 
 test("groups the arrays into an array of arrays of given length, any remainding go in last array", () => {
-    
-    expect(algos.groupArrays(["a", "b", "c", "d"], 4)).toMatchObject([ [ 'a', 'b', 'c', 'd' ] ])
-    expect(algos.groupArrays(["a", "b", "c", "d"], 3)).toMatchObject([ [ 'a', 'b', 'c' ], [ 'd' ] ])
-    expect(algos.groupArrays(["a", "b", "c", "d"], 2)).toMatchObject([ [ 'a', 'b' ], [ 'c', 'd' ] ])
-    
-  })
-
-  
+  expect(algos.groupArrays(["a", "b", "c", "d"], 4)).toMatchObject([
+    ["a", "b", "c", "d"]
+  ])
+  expect(algos.groupArrays(["a", "b", "c", "d"], 3)).toMatchObject([
+    ["a", "b", "c"],
+    ["d"]
+  ])
+  expect(algos.groupArrays(["a", "b", "c", "d"], 2)).toMatchObject([
+    ["a", "b"],
+    ["c", "d"]
+  ])
+})
