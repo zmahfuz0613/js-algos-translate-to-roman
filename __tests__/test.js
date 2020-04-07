03f962ea0daa2f06ec00310b7ab60d08937bb883
Misha Kessler <misha.kessler@gmail.com>
2020-04-07 15:33:52 -0400

    Refactor for name change; add info.yml file.

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,10 @@
-let algos = require("../convertToRoman.js")
+let algos = require("../translateToRoman.js")
 
-test("convert number to Roman Numerals", () => {
-    expect(algos.convertToRoman(180)).toBe('CLXXX')
-    expect(algos.convertToRoman(18)).toBe('XVIII')
-
+test("Translate the given integer to Roman Numerals", () => {
+  expect(algos.translateToRoman(14)).toBe('XIV')
+  expect(algos.translateToRoman(181)).toBe('CLXXXI')
+  expect(algos.translateToRoman(207)).toBe('CCVII')
+  expect(algos.translateToRoman(209)).toBe('CCIX')
+  expect(algos.translateToRoman(1899)).toBe('MDCCCXCIX')
+  expect(algos.translateToRoman(2020)).toBe('MMXX')
 })
